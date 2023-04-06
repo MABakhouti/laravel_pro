ca8df50fc2661851408bce4a4e8b245227d7e131
MABBakhouti <med.bakhouti@gmail.com>
2023-04-06 14:45:39 +0000

    finish add, edit and fixe error upload image :)

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use App\Models\Post;
-use App\Models\Image;
 use Illuminate\Support\Facades\File;
 use App\Models\Categories;
 
